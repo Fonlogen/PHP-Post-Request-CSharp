981f24e5fe2fb62721e6e5792f3f142a1c4f0bd3
Riccardo Reccia <info.fonlogen@gmail.com>
2021-04-16 16:10:59 +0200

    PHP Request page

---
+++ PHP/request.php
@@ -0,0 +1,23 @@
+<?php
+    $cognome = $_POST['examplestring'];
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $name = "example"; // DB name here
+
+    $db_connection = mysqli_connect($host, $user, $pass, $name);
+
+    $query_string = "SELECT examplestring2 FROM clienti WHERE examplestring = '{$examplestring}'";
+    $query_result = $db_connection->query($query_string);
+
+    if ($query_result->num_rows > 0) {
+        while($row = mysqli_fetch_assoc($query_result))
+            echo $row["examplestring2"];
+    } else 
+    {
+        echo "No data"; // Return the No Data if no results were found
+    }
+
+    $db_connection->close();
+?> 
